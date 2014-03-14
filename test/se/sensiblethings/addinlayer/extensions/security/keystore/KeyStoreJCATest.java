6fdae0b7aeac8ca8b144e98f777c92100cfb22be
ailtex <gausszhang@gmail.com>
2014-03-14 14:54:35 +0100

    Adds Junit Test

---
+++ test/se/sensiblethings/addinlayer/extensions/security/keystore/KeyStoreJCATest.java
@@ -0,0 +1,84 @@
+package se.sensiblethings.addinlayer.extensions.security.keystore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KeyStoreJCATest {
+
+	@Test
+	public void testKeyStoreJCA() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadKeyStore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateKeyStore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPublicKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCertificate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrivateKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSecretKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStorePrivateKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStoreSecretKeyStringByteArrayCharArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStoreSecretKeyStringSecretKeyCharArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStoreCertificate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasCertificate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainAlias() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCreationData() {
+		fail("Not yet implemented");
+	}
+
+}
